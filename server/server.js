572fa1bf7228d644e0c4d9e515fd231d5b268b6c
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-04-16 17:27:38 -1000

    replaced front-end hooks to work with Apollo

--- server/server.js
+++ server/server.js
@@ -1,3 +1,35 @@
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// server.applyMiddleware({ app });
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.get('*', (req,res) => {
+//   res.sendFile(path.join(_dirname, '..client/build/index.html'));
+// });
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`🌍 Now listening on localhost:${PORT}`)
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -6,27 +38,41 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+    context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(_dirname, '..client/build/index.html'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
 
